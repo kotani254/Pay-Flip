e46f03ebdb45e0974343f880ee3e3db1ab5cf07a
Kinyabundi <kinyachristine14@gmail.com>
2024-10-14 14:55:20 +0300

    added create account

---
+++ src/app/app/_components/CreateAccount.tsx
@@ -0,0 +1,136 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useAuth } from '@/context/AuthContext';
+import React, { useCallback, useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { InferType, object, string } from "yup";
+import toast from "react-hot-toast";
+import { Controller } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+import {
+    Transaction,
+    TransactionButton,
+    TransactionSponsor,
+    TransactionStatus,
+    TransactionStatusAction,
+    TransactionStatusLabel,
+} from '@coinbase/onchainkit/transaction';
+import type { LifecycleStatus } from '@coinbase/onchainkit/transaction';
+import { base, baseSepolia } from 'wagmi/chains'; 
+import { contractABI, ContractAddress } from '@/constants/contract';
+import { AbiFunction, ContractFunctionParameters } from 'viem';
+
+const personalDetailsSchema = object({
+    name: string().required('Name is required'),
+    phone: string().matches(/^[0-9]+$/, 'Phone number must only contain numbers').required('Phone number is required'),
+}).required();
+
+interface RoleProps {
+    role: "Buyer" | "Merchant";
+}
+
+const PersonalDetails = ({ role }: RoleProps) => {
+    const formMethods = useForm({ resolver: yupResolver(personalDetailsSchema) });
+    const { handleSubmit, reset, control, watch } = formMethods;
+    const { user } = useAuth();
+    const [loading, setLoading] = useState<boolean>(false);
+    const router = useRouter();
+
+    const name = watch('name');
+
+  
+
+    const roleMapping = {
+        Buyer: 0,
+        Merchant: 1,
+    };
+
+    const numericRole = roleMapping[role];
+
+    const transactionContracts: ContractFunctionParameters[] = [
+        {
+        address: ContractAddress,
+        abi: contractABI as unknown as AbiFunction[],
+        functionName: 'createUser',
+        args: [name, numericRole],
+      }
+    ];
+
+    const handleOnStatus = useCallback((status: LifecycleStatus) => {
+        console.log('LifecycleStatus', status);
+        switch (status.statusName) {
+            case 'init':
+            case 'transactionIdle':
+            case 'transactionPending':
+                break;
+            case 'error':
+                toast.error(`Failed to create account: ${status.statusData.message}`);
+                break;
+            case 'transactionLegacyExecuted':
+                toast.success("Transaction submitted. Waiting for confirmation...");
+                break;
+            case 'success':
+                toast.success("Account created successfully!");
+                router.push("/app/merchant/product/dashboard");
+                break;
+            default:
+                console.warn('Unhandled status:', status);
+        }
+    }, [router]);
+
+    return (
+        <div className="flex items-center justify-center h-screen bg-[#B2BEB5]">
+            <div className=" border rounded-xl w-full lg:w-1/2 space-y-4 p-8 flex items-center justify-center ">
+                <div className="px-1 pt-8 flex flex-col items-center justify-center ">
+                    <FormProvider {...formMethods}>
+                        <form className="flex flex-col gap-8 w-full max-w-xs">
+                            <div className="text-2xl text-[#FFF] font-bold mb-4">
+                                Create Merchant Account
+                            </div>
+                            <Controller
+                                control={control}
+                                name="name"
+                                render={({ field }) => (
+                                    <Input
+                                        type="text"
+                                        placeholder="Enter your Name"
+                                        {...field}
+                                    />
+                                )}
+                            />
+                            <Controller
+                                control={control}
+                                name="phone"
+                                render={({ field }) => (
+                                    <Input
+                                        type="tel"
+                                        placeholder="Enter your PhoneNumber"
+                                        {...field}
+                                    />
+                                )}
+                            />
+
+                            <Transaction
+                                 chainId={baseSepolia.id}
+                                 contracts={transactionContracts}
+                                 onStatus={handleOnStatus}
+                            >
+                                <TransactionButton className='bg-[#FFE840]' />
+                                <TransactionSponsor />
+                                <TransactionStatus>
+                                    <TransactionStatusLabel />
+                                    <TransactionStatusAction />
+                                </TransactionStatus>
+                            </Transaction>
+                        </form>
+                    </FormProvider>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PersonalDetails;
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,75 @@
+// contexts/AuthContext.tsx
+
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { useAccount, useConnect, useDisconnect } from 'wagmi';
+import { useRouter } from 'next/navigation';
+import { base, baseSepolia } from 'wagmi/chains'; 
+
+
+interface User {
+  address?: string;
+  chainId?: number;
+  isConnected? : boolean;
+  isConnecting: boolean;
+}
+
+interface AuthContextData {
+  isAuthenticated: boolean;
+  user?: User;
+  login: () => Promise<void>;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState<User>({} as User);
+  const router = useRouter();
+  const {connect, connectors, error, status} = useConnect()
+  const {address, isConnected, isConnecting, isDisconnected} = useAccount()
+  const account = useAccount();
+
+
+  useEffect(() => {
+    const checkAuthenticationStatus = () => {
+      
+      if (account?.isConnected) {
+        setIsAuthenticated(true);
+        setUser({
+          address: account.address,
+          chainId: account.chainId,
+          isConnecting: account.isConnecting,
+          isConnected: account.isConnected
+        });
+      } else {
+        setIsAuthenticated(false);
+        setUser({} as User);
+      }
+    };
+
+    checkAuthenticationStatus();
+  }, [isAuthenticated,address, isConnected, isConnecting ]);
+
+  const login = async () => {
+    try {
+        await connect({chainId: baseSepolia.id, connector: connectors[0]})
+    } catch (error) {
+        console.log(error)
+    }
+  };
+
+  const logout = () => {
+    useDisconnect();
+    setIsAuthenticated(false);
+    setUser({} as User);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
